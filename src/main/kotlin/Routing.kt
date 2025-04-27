78f8ce1ff58100cc90c1f5f130654f5cbbae26b4
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-04-27 10:48:54 +0900

    init

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,11 @@
+package bookstore.playground
+
+import bookstore.playground.handler.member.registerNewMemberHandler
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        post("/members", registerNewMemberHandler)
+    }
+}
---
+++ src/main/kotlin/handler/member/RegisterNewMemberHandler.kt
@@ -0,0 +1,17 @@
+package bookstore.playground.handler.member
+
+import io.ktor.http.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ReceiveNewMember(val emailAddress: String, val name: String)
+
+val registerNewMemberHandler: RoutingHandler = {
+    val receiveNewMember = call.receive<ReceiveNewMember>()
+    call.application.environment.log.info("Received new member registration: $receiveNewMember")
+
+    call.respond(HttpStatusCode.OK)
+}
