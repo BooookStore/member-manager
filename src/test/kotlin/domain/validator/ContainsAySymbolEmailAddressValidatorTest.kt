945162c5fba40e825291bf35488bd68ebb6c2e0d
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-04-27 14:44:06 +0900

    refactor: rename EmailAddressValidatorTest to ContainsAySymbolEmailAddressValidatorTest and add EmailAddressDomainValidatorTest

--- src/test/kotlin/domain/validator/ContainsAySymbolEmailAddressValidatorTest.kt
+++ src/test/kotlin/domain/validator/ContainsAySymbolEmailAddressValidatorTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.fail
 
-class EmailAddressValidatorTest {
+class ContainsAySymbolEmailAddressValidatorTest {
 
     @Test
     fun `include@EmailAddress`() {
@@ -25,28 +25,6 @@ class EmailAddressValidatorTest {
         when (result) {
             is Valid -> fail("Expected Invalid but got Valid")
             is Invalid -> assertEquals(listOf("invalid email address [aaa] not contain @"), result.messages)
-        }
-    }
-
-    @Test
-    fun validDomainEmailAddress() {
-        val validator = EmailAddressDomainValidator("example.com")
-        val result = validator.validate("aaa@example.com")
-
-        when (result) {
-            is Valid -> Unit
-            is Invalid -> fail("Expected Valid but got Invalid")
-        }
-    }
-
-    @Test
-    fun invalidDomainEmailAddress() {
-        val validator = EmailAddressDomainValidator("example.com")
-        val result = validator.validate("aaa@xample.com")
-
-        when (result) {
-            is Valid -> fail("Expected Invalid but got Valid")
-            is Invalid -> assertEquals(listOf("invalid email address [aaa@xample.com] does not match domain [example.com]"), result.messages)
         }
     }
 
---
+++ src/test/kotlin/domain/validator/EmailAddressDomainValidatorTest.kt
@@ -0,0 +1,31 @@
+package bookstore.playground.domain.validator
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
+
+class EmailAddressDomainValidatorTest {
+
+    @Test
+    fun validDomainEmailAddress() {
+        val validator = EmailAddressDomainValidator("example.com")
+        val result = validator.validate("aaa@example.com")
+
+        when (result) {
+            is Valid -> Unit
+            is Invalid -> fail("Expected Valid but got Invalid")
+        }
+    }
+
+    @Test
+    fun invalidDomainEmailAddress() {
+        val validator = EmailAddressDomainValidator("example.com")
+        val result = validator.validate("aaa@xample.com")
+
+        when (result) {
+            is Valid -> fail("Expected Invalid but got Valid")
+            is Invalid -> assertEquals(listOf("invalid email address [aaa@xample.com] does not match domain [example.com]"), result.messages)
+        }
+    }
+
+}
