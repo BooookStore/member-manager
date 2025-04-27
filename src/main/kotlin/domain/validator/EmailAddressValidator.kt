a1547dbcd9018bce6ad0aeb9172da230eab3f295
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-04-27 15:34:44 +0900

    refactor: EmailAddressDomainValidator and ContainsAySymbolEmailAddressValidator

---
+++ src/main/kotlin/domain/validator/EmailAddressValidator.kt
@@ -0,0 +1,25 @@
+package bookstore.playground.domain.validator
+
+class EmailAddressDomainValidator(private val domain: String) : Validator {
+
+    override fun validate(value: String): ValidationResult {
+        return if (value.endsWith("@$domain")) {
+            Valid()
+        } else {
+            Invalid.fromSingleMessage("invalid email address [$value] not match domain [$domain]")
+        }
+    }
+
+}
+
+class ContainsAySymbolEmailAddressValidator : Validator {
+
+    override fun validate(value: String): ValidationResult {
+        return if (value.contains("@")) {
+            Valid()
+        } else {
+            Invalid.fromSingleMessage("invalid email address [$value] not contain '@' symbol")
+        }
+    }
+
+}
