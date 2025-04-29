3f1cebef885a6a953d64f029771fc3f0b1ee2f4b
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-04-29 12:15:40 +0900

    refactor: rename ReceiveNewMember to RequestNewMemberJson and update logging

--- src/main/kotlin/handler/member/RegisterNewMemberHandler.kt
+++ src/main/kotlin/handler/member/RegisterNewMemberHandler.kt
@@ -1,22 +1,21 @@
 package bookstore.playground.handler.member
 
-import bookstore.playground.domain.validator.CompositeValidator
-import bookstore.playground.domain.validator.ContainsAySymbolEmailAddressValidator
-import bookstore.playground.domain.validator.EmailAddressDomainValidator
-import bookstore.playground.domain.validator.Invalid
-import bookstore.playground.domain.validator.Valid
+import bookstore.playground.domain.validator.*
 import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("RegisterNewMemberHandler")!!
 
 @Serializable
-data class ReceiveNewMember(val emailAddress: String, val name: String)
+data class RequestNewMemberJson(val emailAddress: String, val name: String)
 
 suspend fun RoutingContext.registerNewMemberHandler() {
-    val receiveNewMember = call.receive<ReceiveNewMember>()
-    call.application.environment.log.info("Received new member registration: $receiveNewMember")
+    val receiveNewMember = call.receive<RequestNewMemberJson>()
+    logger.info("Received new member request: $receiveNewMember")
 
     val validator = CompositeValidator(ContainsAySymbolEmailAddressValidator, EmailAddressDomainValidator("example.com"))
     val validationResult = validator.validate(receiveNewMember.emailAddress)
