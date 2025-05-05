67f460d2c3012f40dc8afcf87cb358c2f0ad5eab
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-05 21:47:14 +0900

    feat: add Member and EmailAddress classes with validation logic

---
+++ src/main/kotlin/domain/Member.kt
@@ -0,0 +1,34 @@
+package bookstore.playground.domain
+
+import arrow.core.Either
+import arrow.core.EitherNel
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import arrow.core.raise.zipOrAccumulate
+import bookstore.playground.domain.InvalidEmailAddress.Blank
+import bookstore.playground.domain.InvalidEmailAddress.InvalidDomain
+
+data class Member(val name: Name, val emailAddress: EmailAddress)
+
+sealed interface InvalidEmailAddress {
+    object Blank : InvalidEmailAddress
+    object InvalidDomain : InvalidEmailAddress
+}
+
+@ConsistentCopyVisibility
+data class EmailAddress private constructor(val rawEmailAddress: String) {
+
+    companion object {
+        fun create(unvalidatedEmailAddress: UnvalidatedEmailAddress): EitherNel<InvalidEmailAddress, EmailAddress> = either {
+            val rawEmailAddress = unvalidatedEmailAddress.rawEmailAddress
+
+            zipOrAccumulate(
+                { ensure(rawEmailAddress.isNotBlank()) { Blank } },
+                { ensure(rawEmailAddress.substringAfter("@") == "example.com") { InvalidDomain } }
+            ) { _, _ -> EmailAddress(rawEmailAddress) }
+        }
+    }
+
+}
+
+data class Name(val rawName: String)
---
+++ src/test/kotlin/domain/validator/MemberTest.kt
@@ -0,0 +1,63 @@
+package bookstore.playground.domain.validator
+
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.nonEmptyListOf
+import bookstore.playground.domain.EmailAddress
+import bookstore.playground.domain.InvalidEmailAddress
+import bookstore.playground.domain.InvalidEmailAddress.Blank
+import bookstore.playground.domain.InvalidEmailAddress.InvalidDomain
+import bookstore.playground.domain.UnvalidatedEmailAddress
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.fail
+
+class MemberTest {
+
+    @Nested
+    inner class EmailAddressTest {
+
+        @Test
+        fun emailAddressFromUnvalidatedEmailAddress() {
+            val emailAddress = EmailAddress.create(UnvalidatedEmailAddress("john.done@example.com"))
+
+            when (emailAddress) {
+                is Left -> fail("Expected a Either.Right, but got Either.Left")
+                is Right -> emailAddress.value.rawEmailAddress shouldBe "john.done@example.com"
+            }
+        }
+
+        @Test
+        fun invalidEmailAddressFromBlank() {
+            val emailAddress = EmailAddress.create(UnvalidatedEmailAddress(""))
+
+            when (emailAddress) {
+                is Left -> emailAddress.value shouldBe nonEmptyListOf(Blank, InvalidDomain)
+                is Right -> fail("Expected a Either.Left, but got Either.Right")
+            }
+        }
+
+        @Test
+        fun invalidEmailAddressFromNotExampleComDomain() {
+            val emailAddress = EmailAddress.create(UnvalidatedEmailAddress("john.done@xample.com"))
+
+            when (emailAddress) {
+                is Left -> emailAddress.value shouldBe nonEmptyListOf(InvalidDomain)
+                is Right -> fail("Expected a Either.Left, but got Either.Right")
+            }
+        }
+
+        @Test
+        fun invalidEmailAddressFromEmptyAtSymbol() {
+            val emailAddress = EmailAddress.create(UnvalidatedEmailAddress("john.done"))
+
+            when (emailAddress) {
+                is Left -> emailAddress.value shouldBe nonEmptyListOf(InvalidDomain)
+                is Right -> fail("Expected a Either.Left, but got Either.Right")
+            }
+        }
+
+    }
+
+}
