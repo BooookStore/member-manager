943d0f70ebf5177ea5e983a8f7bfdfc09647fcff
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-06 14:36:35 +0900

    feat: implement Member creation with validation and add corresponding tests

--- src/main/kotlin/domain/Member.kt
+++ src/main/kotlin/domain/Member.kt
@@ -6,7 +6,33 @@ import arrow.core.raise.either
 import arrow.core.raise.ensure
 import arrow.core.raise.zipOrAccumulate
 
-data class Member(val name: Name, val emailAddress: EmailAddress)
+sealed interface InvalidMember {
+    data class InvalidMemberName(val invalidName: InvalidName) : InvalidMember
+    data class InvalidMemberEmailAddress(val invalidEmailAddress: InvalidEmailAddress) : InvalidMember
+}
+
+@ConsistentCopyVisibility
+data class Member private constructor(val name: Name, val emailAddress: EmailAddress) {
+
+    companion object {
+        fun create(unvalidatedMember: UnvalidatedMember): EitherNel<InvalidMember, Member> = either {
+            val (unvalidatedName, unvalidatedEmailAddress) = unvalidatedMember
+            zipOrAccumulate(
+                {
+                    Name.create(unvalidatedName)
+                        .mapLeft { InvalidMember.InvalidMemberName(it) }
+                        .bind()
+                },
+                {
+                    EmailAddress.create(unvalidatedEmailAddress)
+                        .mapLeft { it.map(InvalidMember::InvalidMemberEmailAddress) }
+                        .bindNel()
+                }
+            ) { name, emailAddress -> Member(name, emailAddress) }
+        }
+    }
+
+}
 
 sealed interface InvalidEmailAddress {
     object Blank : InvalidEmailAddress
--- src/test/kotlin/domain/validator/MemberTest.kt
+++ src/test/kotlin/domain/validator/MemberTest.kt
@@ -11,6 +11,58 @@ import kotlin.test.fail
 
 class MemberTest {
 
+    @Test
+    fun memberFromValidEmailAddressAndName() {
+        val member = Member.create(
+            UnvalidatedMember(
+                UnvalidatedName("John Doe"),
+                UnvalidatedEmailAddress("john.doe@example.com")
+            )
+        )
+
+        when (member) {
+            is Left -> fail("Expected a Either.Right, but got Either.Left")
+            is Right -> {
+                member.value.name.rawName shouldBe "John Doe"
+                member.value.emailAddress.rawEmailAddress shouldBe "john.doe@example.com"
+            }
+        }
+    }
+
+    @Test
+    fun memberFromInvalidName() {
+        val member = Member.create(
+            UnvalidatedMember(
+                UnvalidatedName(" "),
+                UnvalidatedEmailAddress("john.doe@example.com")
+            )
+        )
+
+        when (member) {
+            is Left -> member.value shouldBe nonEmptyListOf(InvalidMember.InvalidMemberName(InvalidName.Blank))
+            is Right -> fail("Expected a Either.Left, but got Either.Right")
+        }
+    }
+
+    @Test
+    fun memberFromInvalidNameAndEmailAddress() {
+        val member = Member.create(
+            UnvalidatedMember(
+                UnvalidatedName(" "),
+                UnvalidatedEmailAddress("")
+            )
+        )
+
+        when (member) {
+            is Left -> member.value shouldBe nonEmptyListOf(
+                InvalidMember.InvalidMemberName(InvalidName.Blank),
+                InvalidMember.InvalidMemberEmailAddress(InvalidEmailAddress.Blank),
+                InvalidMember.InvalidMemberEmailAddress(InvalidEmailAddress.InvalidDomain)
+            )
+            is Right -> fail("Expected a Either.Left, but got Either.Right")
+        }
+    }
+
     @Nested
     inner class EmailAddressTest {
 
