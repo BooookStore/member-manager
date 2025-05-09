2c94f8ffa38bbbc1f4e7520ae5b26587e9ded144
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-09 18:24:23 +0900

    feat: implement E2E test base class for PostgreSQL integration

--- src/test/kotlin/e2e/RegisterNewMemberTest.kt
+++ src/test/kotlin/e2e/RegisterNewMemberTest.kt
@@ -1,4 +1,4 @@
-package bookstore.playground.e2e
+package e2e
 
 import bookstore.playground.module
 import io.kotest.matchers.should
@@ -11,17 +11,8 @@ import io.ktor.serialization.jackson.*
 import io.ktor.server.testing.*
 import kotlinx.coroutines.test.runTest
 import net.javacrumbs.jsonunit.kotest.equalJson
-import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.junit.jupiter.api.TestInfo
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
-import org.testcontainers.utility.MountableFile
-import java.io.File
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -41,62 +32,7 @@ fun testApplicationWithCommonSetup(block: suspend ApplicationTestBuilder.(client
     }
 
 @Testcontainers
-class RegisterNewMemberTest {
-
-    private val postgresUser = "postgres"
-
-    private val postgresPassword = "postgres"
-
-    @Container
-    val postgres = GenericContainer(DockerImageName.parse("postgres:17.4-bookworm"))
-        .withExposedPorts(5432)
-        .withEnv("POSTGRES_USER", postgresUser)
-        .withEnv("POSTGRES_PASSWORD", postgresPassword)
-        .withEnv("POSTGRES_DB", "member-manager")
-        .withCopyFileToContainer(MountableFile.forClasspathResource("database/01-schema.sql"), "/docker-entrypoint-initdb.d/01-schema.sql")
-        .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 2))!!
-
-    @BeforeTest
-    fun setUp(testInfo: TestInfo) {
-        connectToDatabase()
-        truncateDatabase()
-        loadDataToDatabase(testInfo)
-    }
-
-    private fun loadDataToDatabase(testInfo: TestInfo) {
-        val testClassResourceDir = testInfo.testClass.map { it.name }
-            .map { it.replace("bookstore.playground", "").replace(".", "/").substring(1) }
-            .orElseThrow { IllegalArgumentException("Test class not found") }
-
-        val testMethodResourceDir = testInfo.testMethod.map { it.name }
-            .orElseThrow { IllegalArgumentException("Test method not found") }
-
-        val testResourceDir = "$testClassResourceDir/$testMethodResourceDir"
-        println("test resource dir: $testResourceDir")
-
-        File("src/test/resources/$testResourceDir").listFiles()?.forEach { file ->
-            println("test resource file: ${file.name}")
-            loadFileToDatabase(file)
-        }
-    }
-
-    private fun loadFileToDatabase(file: File) = file.bufferedReader().use { reader ->
-        println("test resource content: ${reader.readText()}")
-        transaction { exec(reader.readText()) }
-    }
-
-    private fun truncateDatabase() = transaction {
-        exec("TRUNCATE TABLE member")
-    }
-
-    private fun connectToDatabase() {
-        Database.connect(
-            url = "jdbc:postgresql://${postgres.host}:${postgres.firstMappedPort}/member-manager",
-            user = postgresUser,
-            password = postgresPassword,
-            driver = "org.postgresql.Driver",
-        )
-    }
+class RegisterNewMemberTest : E2ETestBase() {
 
     @Test
     fun testContainerIsRunning() = runTest {
