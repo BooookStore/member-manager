883fe514f61dce70c45064b0b5fe67b7c4050a7f
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-09 19:47:36 +0900

    test: remove unnecessary test for container state in RegisterNewMemberTest

--- src/test/kotlin/e2e/RegisterNewMemberTest.kt
+++ src/test/kotlin/e2e/RegisterNewMemberTest.kt
@@ -1,11 +1,9 @@
 package e2e
 
 import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlinx.coroutines.test.runTest
 import net.javacrumbs.jsonunit.kotest.equalJson
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -14,20 +12,6 @@ import kotlin.test.assertEquals
 
 @Testcontainers
 class RegisterNewMemberTest : E2ETestBase() {
-
-    @Test
-    fun testContainerIsRunning() = runTest {
-        transaction {
-            exec("SELECT * FROM member") { result ->
-                while (result.next()) {
-                    println("member name: ${result.getString("name")}")
-                    assertEquals("John Doe", result.getString("name"))
-                    println("member email address: ${result.getString("email_address")}")
-                    assertEquals("john.doe@example.com", result.getString("email_address"))
-                }
-            }
-        }
-    }
 
     @Test
     fun validMemberReturnedCreated() = testApplicationWithCommonSetup { client ->
