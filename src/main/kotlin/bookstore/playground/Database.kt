a5e08754bd0b16f9ece5ee6aaa62ae18a86b0d2d
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-12 16:12:52 +0900

    refactor: add database configuration and update application module path

---
+++ src/main/kotlin/bookstore/playground/Database.kt
@@ -0,0 +1,17 @@
+package bookstore.playground
+
+import io.ktor.server.application.Application
+import org.jetbrains.exposed.sql.Database
+
+fun Application.configureDatabase() {
+    val url = environment.config.property("postgres.url").getString()
+    val user = environment.config.property("postgres.user").getString()
+    val password = environment.config.property("postgres.password").getString()
+
+    Database.connect(
+        url = url,
+        user = user,
+        password = password,
+        driver = "org.postgresql.Driver",
+    )
+}
