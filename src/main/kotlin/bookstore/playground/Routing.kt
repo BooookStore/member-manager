e87b1fd83d7f47cbba199df60c8739f5c9c9da39
BooookStore <6366129+BooookStore@users.noreply.github.com>
2025-05-24 12:13:18 +0900

    feat: add GET endpoint for retrieving member by ID and implement corresponding test

--- src/main/kotlin/bookstore/playground/Routing.kt
+++ src/main/kotlin/bookstore/playground/Routing.kt
@@ -1,13 +1,18 @@
 package bookstore.playground
 
 import bookstore.playground.handler.registerNewMemberHandler
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         post("/members") {
             registerNewMemberHandler()
+        }
+        get("/members/{id}") {
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
---
+++ src/test/kotlin/bookstore/playground/e2e/GetMemberTest.kt
@@ -0,0 +1,18 @@
+package bookstore.playground.e2e
+
+import io.kotest.matchers.shouldBe
+import io.ktor.client.request.*
+import io.ktor.http.HttpStatusCode
+import kotlin.test.Test
+
+class GetMemberTest : E2ETestBase() {
+
+    @Test
+    fun getMember() = testApplicationWithCommonSetup { client ->
+        client.get("/members/1bfafdec-16e5-4ff8-a8ab-146e0e6bb215")
+        .apply {
+            status shouldBe HttpStatusCode.OK
+        }
+    }
+
+}
